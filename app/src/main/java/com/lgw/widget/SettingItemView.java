0daccdecfafad261267623e6f4d62772c4ccfa3b
十年一城 <986114497@qq.com>
2018-06-18 21:09:51 +0800

    添加自定义view

---
+++ app/src/main/java/com/lgw/widget/SettingItemView.java
@@ -0,0 +1,60 @@
+package com.lgw.widget;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import com.lgw.R;
+
+public class SettingItemView extends RelativeLayout {
+
+    TextView tv_title,tv_desc;
+
+    CheckBox cb_status;
+
+    String desc_on, desc_off;
+
+    public SettingItemView(Context context) {
+        this(context,null);
+    }
+
+    public SettingItemView(Context context, AttributeSet attrs) {
+        super(context, attrs,0);
+    }
+
+    public SettingItemView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        View view = View.inflate(context, R.layout.setting_item_view, null);
+        tv_title = view.findViewById(R.id.tv_title);
+        tv_desc = view.findViewById(R.id.tv_desc);
+        cb_status = view.findViewById(R.id.cb_status);
+
+
+        TypedArray ta = context.obtainStyledAttributes(attrs, R.styleable.SettingItemView);
+        String title = ta.getString(R.styleable.SettingItemView_setting_title);
+        desc_on = ta.getString(R.styleable.SettingItemView_desc_on);
+        desc_off = ta.getString(R.styleable.SettingItemView_desc_off);
+        tv_title.setText(title);
+
+        ta.recycle();
+
+    }
+
+    public boolean isChecked(){
+        return cb_status.isChecked();
+    }
+
+    public void setChecked(boolean checked){
+        if(checked){
+            tv_desc.setText(desc_on);
+        }else{
+            tv_desc.setText(desc_off);
+        }
+        cb_status.setChecked(checked);
+    }
+
+
+}
