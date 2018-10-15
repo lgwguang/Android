40d2798f5a408316cae6c78ab2d7c61bd58396df
986114497 <986114497@qq.com>
2018-10-15 16:00:20 +0800

    201810151003

--- app/src/main/java/com/lgw/mvp/ILoginModel.java
+++
@@ -1,8 +0,0 @@
-package com.lgw.mvp;
-
-import org.json.JSONObject;
-
-public interface ILoginModel extends IModel{
-
-    JSONObject login(String callback);
-}
--- app/src/main/java/com/lgw/mvp/IModel.java
+++
@@ -1,6 +0,0 @@
-package com.lgw.mvp;
-
-public interface IModel {
-
-    void onDestroy();
-}
